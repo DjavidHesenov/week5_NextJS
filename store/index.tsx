aca78886d9eebfdcfd70b837aeeda8ebbdf8a310
Djavid <hesenovdjavid@gmail.com>
2021-12-27 01:54:38 +0400

    slight change

--- store/index.tsx
+++ store/index.tsx
@@ -1,10 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counter'
 import authReducer from './auth'
 
 
 const store = configureStore({
-    reducer: { counter: counterReducer, auth: authReducer }
+    reducer: { auth: authReducer }
 })
 
 export default store
